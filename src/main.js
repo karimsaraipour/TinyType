983ec7a49167e1d1b634762af843814b9d595845
karimsaraipour <75566475+karimsaraipour@users.noreply.github.com>
2022-04-06 13:40:34 -0700

    add letter divs

--- src/main.js
+++ src/main.js
@@ -3,5 +3,19 @@ var TINYTYPE = TINYTYPE || {}
 
 $(document).ready(() => {
     console.log('Initialize TinyType')
+    // Calling the function
+    let alphabet = () => {
+        let alphabet = [];
+        for (i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
+            alphabet.push(String.fromCharCode(i));
+        }
+        return alphabet
+    }
+    
+    console.log(alphabet.call())
+    // Printing the array inside the .letters element
+    $.each(alphabet.call(), (index, element) => {
+        $("#keyboard").append("<td><div class='letter'>" + element + "</div></td>");
+    });
     
 })
