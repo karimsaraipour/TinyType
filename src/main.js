f21e7040022ae90d6482bc9349751f00fbeff0da
karimsaraipour <75566475+karimsaraipour@users.noreply.github.com>
2022-04-08 13:41:25 -0700

    add basic typing

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ var TINYTYPE = TINYTYPE || {}
 
 $(document).ready(() => {
     console.log('Initialize TinyType')
-    // Calling the function
+    // modify basic html to have divs for alphabet
     let alphabet = () => {
         let alphabet = [];
         for (i = 'a'.charCodeAt(0); i <= 'z'.charCodeAt(0); i++) {
@@ -12,11 +12,39 @@ $(document).ready(() => {
         return alphabet
     }
     
-    console.log(alphabet.call())
-    // Printing the array inside the .letters element
     $.each(alphabet.call(), (index, element) => {
-        $("#keyboard").append("<td><div class='letter'>" + element + "</div></td>");
-    });
-    
+        $("#keyboard").append("<td><div class='letter' id='" + element + "'>" + element + "</div></td>");
+        $("#" + element).on('mousedown', {letter: element}, TINYTYPE.typeLetter);
 
+    });
+
+    $("#backspace").on('mousedown', TINYTYPE.deleteOne);
+    
 })
+
+
+TINYTYPE.typeLetter = (event) => {
+    let currentTextField = $("#textfield").val();
+    let letter = event.data.letter;
+    console.log(letter)
+    let newTextField = currentTextField + letter;
+    $("#textfield").val(newTextField);
+}
+
+TINYTYPE.deleteOne = () => {
+    let currentTextField = $("#textfield").val();
+    let newTextField = currentTextField.substring(0, currentTextField.length - 1);
+    $("#textfield").val(newTextField);
+}
+
+TINYTYPE.deleteWord = () => {
+    let currentTextField = $("#textfield").val();
+    let lastSpace = currentTextField.lastIndexOf(" ")
+    if (lastSpace == -1) {
+        $("#textfield").val("");
+    }
+    else {
+        let newTextField = currentTextField.substring(0, lastSpace);
+        $("#textfield").val(newTextField);
+    }
+}
